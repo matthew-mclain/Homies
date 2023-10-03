057031bd9aaa8aeaecf89f62d4a3e68377f74264
Matthew McLain <mwm.mclain@gmail.com>
2023-10-02 22:05:19 -0400

    Started onCreate() in LoginActivity

--- app/src/main/java/com/example/homies/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/homies/ui/login/LoginActivity.java
@@ -1,4 +1,15 @@
 package com.example.homies.ui.login;
 
-public class LoginActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.homies.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
