0a12c8b55d59cd39298c27e4f5a000f9ad6794ba
Cindy Liao <>
2023-10-03 14:06:21 -0400

    Added Firebase to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,4 +51,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    //add other SDKs that we want to use later (https://firebase.google.com/docs/android/setup#available-libraries)
 }
