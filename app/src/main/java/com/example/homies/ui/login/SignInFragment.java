42428968da78dfe84794ad1fca61693ee8172eb3
SangeunLeeKorea <sarahvison8@gmail.com>
2023-10-05 23:22:46 -0400

    Sign up finished

--- app/src/main/java/com/example/homies/ui/login/SignInFragment.java
+++ app/src/main/java/com/example/homies/ui/login/SignInFragment.java
@@ -16,14 +16,18 @@ public class SignInFragment extends Fragment implements View.OnClickListener{
 
     EditText usernameET;
     EditText passwordET;
+    LoginActivity loginActivity;
+    View view;
 
-    public SignInFragment() {
+    public SignInFragment(LoginActivity loginActivity) {
+        this.loginActivity = loginActivity;
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+        view = inflater.inflate(R.layout.fragment_signin, container, false);
 
         usernameET = view.findViewById((R.id.editTextSignInUsername));
         passwordET = view.findViewById(R.id.editTextSignInPassword);
@@ -33,28 +37,38 @@ public class SignInFragment extends Fragment implements View.OnClickListener{
         if (signInButton != null){
             signInButton.setOnClickListener(this);
         }
+        final Button backButton = view.findViewById(R.id.buttonBack);
+        if (backButton != null){
+            backButton.setOnClickListener(this);
+        }
 
         return view;
     }
 
     @Override
     public void onClick(View view){
-        String username = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
-        FragmentManager manager = getParentFragmentManager();
+        if (view.getId() == R.id.buttonSignInSubmit){
+            String username = usernameET.getText().toString();
+            String password = passwordET.getText().toString();
+            FragmentManager manager = getParentFragmentManager();
 
-        //need to check from database for 1. if id exists 2. if password matches with id info
-        if (username.equals("a") && password.equals("1")){
-            //login success
-        } else if (username.equals("") || password.equals("")) {
-            //if empty field, error dialog (type 0)
-            SignInErrorDialogFragment dialog = new SignInErrorDialogFragment(0);
-            dialog.show(manager, "SignInError");
-        } else {
-            //if login fail, error dialog (type 1)
-            SignInErrorDialogFragment dialog = new SignInErrorDialogFragment(1);
-            dialog.show(manager, "SignInError");
+            //need to check from database for 1. if id exists 2. if password matches with id info
+            if (username.equals("a") && password.equals("1")){
+                //login success
+                //need to switch activity
+            } else if (username.equals("") || password.equals("")) {
+                //if empty field, error dialog (type 0)
+                SignInErrorDialogFragment dialog = new SignInErrorDialogFragment(0);
+                dialog.show(manager, "SignInError");
+            } else {
+                //if login fail, error dialog (type 1)
+                SignInErrorDialogFragment dialog = new SignInErrorDialogFragment(1);
+                dialog.show(manager, "SignInError");
+            }
+        } else if (view.getId() == R.id.buttonBack){
+            loginActivity.showButtons(this.view);
         }
+
     }
 }
 
--- app/src/main/java/com/example/homies/ui/login/SignUpFragment.java
+++ app/src/main/java/com/example/homies/ui/login/SignUpFragment.java
@@ -1,5 +1,9 @@
 package com.example.homies.ui.login;
 
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,19 +16,22 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.homies.R;
 
-public class SignUpFragment extends Fragment implements View.OnClickListener{
+public class SignUpFragment extends Fragment implements View.OnClickListener, DialogInterface.OnClickListener{
 
     EditText usernameET;
     EditText passwordET;
     EditText passwordConfirmET;
+    LoginActivity loginActivity;
+    View view;
 
-    public SignUpFragment() {
+    public SignUpFragment(LoginActivity loginActivity) {
+        this.loginActivity = loginActivity;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        view = inflater.inflate(R.layout.fragment_signup, container, false);
 
         usernameET = view.findViewById((R.id.editTextSignInUsername));
         passwordET = view.findViewById(R.id.editTextSignInPassword);
@@ -35,36 +42,59 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
         if (signInButton != null){
             signInButton.setOnClickListener(this);
         }
+        final Button backButton = view.findViewById(R.id.buttonBack);
+        if (backButton != null){
+            backButton.setOnClickListener(this);
+        }
+
 
         return view;
     }
 
     @Override
     public void onClick(View view){
+        if (view.getId() == R.id.buttonSignUpSubmit){
+            String username = usernameET.getText().toString();
+            String password = passwordET.getText().toString();
+            String passwordConfirm = passwordConfirmET.getText().toString();
+            FragmentManager manager = getParentFragmentManager();
 
-        String username = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
-        String passwordConfirm = passwordConfirmET.getText().toString();
-        FragmentManager manager = getParentFragmentManager();
 
+            //check if any field is empty
+            if (password.equals("") || passwordConfirm.equals("") || username.equals("")){
+                // if yes for any, error dialog (type 0)
+                SignUpErrorDialogFragment dialog = new SignUpErrorDialogFragment(0);
+                dialog.show(manager, "SignInError");
+            }
+            //check if two passwords matches
+            else if (!password.equals(passwordConfirm)) {
+                //if not, error dialog (type 1)
+                SignUpErrorDialogFragment dialog = new SignUpErrorDialogFragment(1);
+                dialog.show(manager, "SignInError");
+            }
+            //check if username is already taken
 
-        //check if any field is empty
-        if (password.equals("") || passwordConfirm.equals("") || username.equals("")){
-            // if yes for any, error dialog (type 0)
-            SignUpErrorDialogFragment dialog = new SignUpErrorDialogFragment(0);
-            dialog.show(manager, "SignInError");
+            //sign up completed
+            else {
+                new AlertDialog.Builder(requireActivity())
+                        .setTitle("Congratulations!")
+                        .setMessage("Successfully signed up for Homies!\nMove to Sign In page?")
+                        .setNegativeButton("NO", this)
+                        .setPositiveButton("YES", this)
+                        .create().show();
+            }
+
+        } else if (view.getId() == R.id.buttonBack){
+            loginActivity.showButtons(this.view);
         }
-        //check if two passwords matches
-        else if (!password.equals(passwordConfirm)) {
-            //if not, error dialog (type 1)
-            SignUpErrorDialogFragment dialog = new SignUpErrorDialogFragment(1);
-            dialog.show(manager, "SignInError");
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        loginActivity.showButtons(this.view);
+        if (which == BUTTON_POSITIVE){
+            loginActivity.showSignInFragment();
         }
-        //check if username is already taken
-
-        //sign up completed
-
-
     }
 }
 
