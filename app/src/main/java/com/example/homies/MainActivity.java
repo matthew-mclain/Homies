bc59ca0e1b5be34a82718e97ae1adb96d8504f10
Cindy Liao <>
2023-10-18 12:57:05 -0400

    Added drawer navigation

--- app/src/main/java/com/example/homies/MainActivity.java
+++ app/src/main/java/com/example/homies/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.homies;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.homies.ui.messages.MessagesFragment;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //drawer layout instance to toggle the menu icon to open
+        //drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        //pass the open and close toggle for the drawer layout listener to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Replace the initial fragment (MessagesFragment)
         replaceFragment(new MessagesFragment());
@@ -29,5 +47,19 @@ public class MainActivity extends AppCompatActivity {
     // Call this method when a bottom navigation item is clicked
     public void openFragment(Fragment fragment) {
         replaceFragment(fragment);
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
