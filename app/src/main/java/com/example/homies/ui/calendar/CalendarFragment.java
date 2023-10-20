af0b5c93d12d756f01c9b7e5bf30ca7ea09f9415
Matthew McLain <mwm.mclain@gmail.com>
2023-10-20 18:38:16 -0400

    Fixed calendar and laundry layouts. Inflated them in fragments.

--- app/src/main/java/com/example/homies/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/homies/ui/calendar/CalendarFragment.java
@@ -1,12 +1,32 @@
 package com.example.homies.ui.calendar;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.homies.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import timber.log.Timber;
+
 public class CalendarFragment extends Fragment implements View.OnClickListener {
+
+    View view;
+    private static FirebaseFirestore db;
+    private final String TAG = getClass().getSimpleName();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        Timber.tag(TAG).d("onCreateView()");
+
+        return view;
+    }
+
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
     }
 }
--- app/src/main/java/com/example/homies/ui/laundry/LaundryFragment.java
+++ app/src/main/java/com/example/homies/ui/laundry/LaundryFragment.java
@@ -1,12 +1,32 @@
 package com.example.homies.ui.laundry;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.homies.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import timber.log.Timber;
+
 public class LaundryFragment extends Fragment implements View.OnClickListener {
+    View view;
+    private static FirebaseFirestore db;
+    private final String TAG = getClass().getSimpleName();
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_laundry, container, false);
+        Timber.tag(TAG).d("onCreateView()");
+
+        return view;
+    }
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
     }
 }
