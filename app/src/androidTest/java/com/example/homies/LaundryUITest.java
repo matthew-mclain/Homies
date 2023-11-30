0f24488ca05068536c3462ff65d9c17dd543bef5
Matthew McLain <mwm.mclain@gmail.com>
2023-11-29 22:47:26 -0500

    Fixed Laundry UI test

--- app/src/androidTest/java/com/example/homies/LaundryUITest.java
+++ app/src/androidTest/java/com/example/homies/LaundryUITest.java
@@ -6,17 +6,27 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -27,6 +37,74 @@ public class LaundryUITest {
 
     @Test
     public void laundryUITest(){
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.buttonSignIn), withText("Sign In"),
+                        childAtPosition(
+                                allOf(withId(R.id.fragment_container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editTextSignInEmail),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_container),
+                                        3),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("liao.563@osu.edu"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editTextSignInPassword),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_container),
+                                        3),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("1234567"), closeSoftKeyboard());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.buttonSignInSubmit), withText("Sign In"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_container),
+                                        3),
+                                2),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open"),
+                        childAtPosition(
+                                allOf(withId(androidx.appcompat.R.id.action_bar),
+                                        childAtPosition(
+                                                withId(androidx.appcompat.R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                                allOf(withId(com.google.android.material.R.id.design_navigation_view),
+                                        childAtPosition(
+                                                withId(R.id.nav_view),
+                                                0)),
+                                5),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
 
         try{
             Thread.sleep(5215);
@@ -97,65 +175,24 @@ public class LaundryUITest {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
 
-        //press stop to stop laundry
-        ViewInteraction stopButton = onView(
-                allOf(withId(R.id.laundryStopButton), withText("Stop"), isDisplayed()));
-        stopButton.perform(click());
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
 
-        //short break to check result
-        try{
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
 
-        //press edit to edit machine
-        ViewInteraction editButton = onView(
-                allOf(withId(R.id.machineEditButton), withText("Edit"), isDisplayed()));
-        editButton.perform(click());
-
-        //press cancel
-        ViewInteraction cancelEditButton = onView(
-                allOf(withId(R.id.cancelMachineEditButton), withText("Cancel"), isDisplayed()));
-        cancelEditButton.perform(click());
-
-        //Press edit again
-        editButton.perform(click());
-
-        //change machine name
-        ViewInteraction editTextMachineNameToChange = onView(
-                allOf(withId(R.id.editTextMachineNameToEdit), isDisplayed()));
-        editTextMachineNameToChange.perform(typeText(" CHANGED"));
-
-        //press apply
-        ViewInteraction applyChangeButton = onView(
-                allOf(withId(R.id.applyMachineEditButton), withText("Apply"), isDisplayed()));
-        applyChangeButton.perform(click());
-
-        //short break to check result
-        try{
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-        //press edit again
-        editButton.perform(click());
-
-        //press delete machine
-        ViewInteraction deleteButton = onView(
-                allOf(withId(R.id.deleteMachineButton), withText("Delete Machine"), isDisplayed()));
-        deleteButton.perform(click());
-
-        //short break to check result
-        try{
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
